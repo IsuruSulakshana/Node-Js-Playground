7b605b26af0ce16a37187ae74d512b143de927a8
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-12-30 21:22:59 +0530

    learn and code event loop

---
+++ 1-event-loop-examples/2-setTimout.js
@@ -0,0 +1,10 @@
+//started operating system process
+console.log('first')
+setTimeout(()=>{
+    console.log('second')
+},2)
+setTimeout(()=>{
+    console.log('fourth')
+},1)
+console.log('third')
+//complete and exited operating system
--- 2-async-patterns/1-block.js
+++ 2-async-patterns/1-block.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   }
   if (req.url === '/about') {
     // blocking code
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 1000; i++) {
       for (let j = 0; j < 1000; j++) {
         console.log(`${i} ${j}`)
       }
@@ -19,4 +19,3 @@ const server = http.createServer((req, res) => {
 server.listen(5000, () => {
   console.log('Server listening on port : 5000....')
 })
-
