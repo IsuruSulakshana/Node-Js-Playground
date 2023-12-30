047d029399be6e8ec066d79cc23717e8e0ddae20
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-12-30 22:19:55 +0530

    updated

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-const http = require('http')
+const {readFile} = require('fs')
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Home Page')
-  }
-  if (req.url === '/about') {
-    // blocking code
-    for (let i = 0; i < 100; i++) {
-      for (let j = 0; j < 1000; j++) {
-        console.log(`${i} ${j}`)
-      }
+readFile('./content/first.txt', 'utf8', (err, data)=>{
+    if (err) {
+        return
+    } else {
+        console.log(data)
     }
-    res.end('About Page')
-  }
-  res.end('Error Page')
 })
-
-server.listen(5000, () => {
-  console.log('Server listening on port : 5000....')
-})
-
