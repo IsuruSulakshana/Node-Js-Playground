3440cbf5ec383a9c9d177c2e3dd1ec329055d7e3
Isuru Sulakshana <pererawhis.20@uom.lk>
2024-01-06 23:36:14 +0530

    updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const start = async() => {
     try {
         const first = await readFilePromise('./content/first.txt', 'utf8')
         const second = await readFilePromise('./content/second.txt', 'utf8')
-        await writeFilePromise('./content/result-mind-grenade.txt', 'THIS IS AWESOME : ')
+        await writeFilePromise('./content/result-mind-grenade.txt', `THIS IS AWESOME : ${first}. ${second}.`)
         console.log(first)
         console.log(second)
     } catch (error) {
